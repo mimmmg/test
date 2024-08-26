f49f69a8abd7abff75b2d28050201d013eeec3d6
Goo Min Ji <oguma20@gmail.com>
2024-08-26 11:32:48 +0900

    0823 am

--- obj5.js
+++ obj5.js
@@ -5,11 +5,31 @@ abstract class Animal {
         this.name = name;
     }
 
-    //추상매서드
+    //추상 메서드
     abstract void sound();
 
-    //일반매서드
+    // 일반 메서드
     void sleep() {
-        System.out
+        System.out.println(name + "is sleeping.");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name) {
+        super(name);
+    }
+
+    // 추상 메서드 구현
+    @Override
+    void sound() {
+        System.out.println("Bark");
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy");
+        dog.sound();  //출력: Bark
+        dog.sleep();  //출력: Buddy is sleeping.
     }
 }
